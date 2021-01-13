195293c98f9b40ec78c498c9e6d02b01df82ce8b
kenpg1989 <ken98jazz@yahoo.co.jp>
2021-01-14 07:18:16 +0900

    no message

--- web.php
+++ web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Route::get('/hello', 'App\Http\Controllers\HelloController@index');
 Route::get('view', 'App\Http\Controllers\HelloController@view');
 Route::get('list', 'App\Http\Controllers\HelloController@list');
+Route::get('escape', 'App\Http\Controllers\ViewController@escape');
+Route::get('if', 'App\Http\Controllers\ViewController@if');
+Route::get('unless', 'App\Http\Controllers\ViewController@unless');
+Route::get('foreach_assoc', 'App\Http\Controllers\ViewController@foreach_assoc');
+Route::get('foreach_loop', 'App\Http\Controllers\ViewController@foreach_loop');
+Route::get('master', 'App\Http\Controllers\ViewController@master');
+Route::get('comp', 'App\Http\Controllers\ViewController@comp');
+Route::get('book', 'App\Http\Controllers\ViewController@book');
